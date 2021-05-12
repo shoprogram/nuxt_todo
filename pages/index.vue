b9da69a186be89fa7c5629016cfeb5f91bfac50d
高橋茉由 <>
2021-05-12 18:40:30 +0900

    コンポーネント 化したものを一旦ベタ書きに戻した

--- pages/index.vue
+++ pages/index.vue
@@ -2,31 +2,229 @@
   <main>
     <ol class="categories">
       <li class="category category__work">
-        <TodoCategory
-          :target="categoryList.work"
-          @toggle-modal="toggleModal"
-          @showEditModal="showEditModal"
-          @finishedTodo="finishedTodo"
-          @showDeleteModal="showDeleteModal"
-        ></TodoCategory>
+        <div>
+          <div class="category-title">
+            <label>Work</label>
+          </div>
+          <draggable
+            v-model="draggableListWork"
+            element="ul"
+            draggable=".todo"
+            group="todos"
+          >
+            <li
+              v-for="(item, i) in draggableListWork"
+              :key="i"
+              class="todo"
+              @mouseover="
+                isShowEditIcon.work = true
+                currentIndex = i
+              "
+              @mouseleave="
+                isShowEditIcon.work = false
+                currentIndex = -1
+              "
+            >
+              <div class="todo__title">
+                <v-checkbox
+                  class="checkbox mt-0 pt-0"
+                  :off-icon="'mdi-checkbox-blank-circle-outline'"
+                  :on-icon="'mdi-check-circle-outline'"
+                  color="red darken-3"
+                  dense
+                  @click="
+                    finishedTodo({
+                      index: i,
+                      category: 'work',
+                    })
+                  "
+                ></v-checkbox>
+                <label :class="{ finished: checkFinished(item) }">{{
+                  item.title
+                }}</label>
+              </div>
+
+              <div
+                v-show="isShowEditIcon[item.category] && currentIndex === i"
+                class="todo__menu"
+              >
+                <div
+                  class="edit-button"
+                  @click="
+                    showEditModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-lead-pencil</v-icon>
+                </div>
+                <div
+                  class="delete-button"
+                  @click="
+                    showDeleteModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-delete</v-icon>
+                </div>
+              </div>
+            </li>
+          </draggable>
+          <AddButton @click="toggleModal('work')"></AddButton>
+        </div>
       </li>
       <li class="category category__private">
-        <TodoCategory
-          :target="categoryList.private"
-          @toggle-modal="toggleModal"
-          @showEditModal="showEditModal"
-          @finishedTodo="finishedTodo"
-          @showDeleteModal="showDeleteModal"
-        ></TodoCategory>
+        <div>
+          <div class="category-title">
+            <label>Private}</label>
+          </div>
+          <draggable
+            v-model="draggableListPrivate"
+            element="ul"
+            draggable=".todo"
+            group="todos"
+          >
+            <li
+              v-for="(item, i) in draggableListPrivate"
+              :key="i"
+              class="todo"
+              @mouseover="
+                isShowEditIcon[item.category] = true
+                currentIndex = i
+              "
+              @mouseleave="
+                isShowEditIcon[item.category] = false
+                currentIndex = -1
+              "
+            >
+              <div class="todo__title">
+                <v-checkbox
+                  class="checkbox mt-0 pt-0"
+                  :off-icon="'mdi-checkbox-blank-circle-outline'"
+                  :on-icon="'mdi-check-circle-outline'"
+                  color="red darken-3"
+                  dense
+                  @click="
+                    finishedTodo({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                ></v-checkbox>
+                <label :class="{ finished: checkFinished(item) }">{{
+                  item.title
+                }}</label>
+              </div>
+
+              <div
+                v-show="isShowEditIcon[item.category] && currentIndex === i"
+                class="todo__menu"
+              >
+                <div
+                  class="edit-button"
+                  @click="
+                    showEditModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-lead-pencil</v-icon>
+                </div>
+                <div
+                  class="delete-button"
+                  @click="
+                    showDeleteModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-delete</v-icon>
+                </div>
+              </div>
+            </li>
+          </draggable>
+          <AddButton @click="toggleModal('private')"></AddButton>
+        </div>
       </li>
       <li class="category category__random">
-        <TodoCategory
-          :target="categoryList.random"
-          @toggle-modal="toggleModal"
-          @showEditModal="showEditModal"
-          @finishedTodo="finishedTodo"
-          @showDeleteModal="showDeleteModal"
-        ></TodoCategory>
+        <div>
+          <div class="category-title">
+            <label>Random</label>
+          </div>
+          <draggable
+            v-model="draggableListRandom"
+            element="ul"
+            draggable=".todo"
+            group="todos"
+          >
+            <li
+              v-for="(item, i) in draggableListRandom"
+              :key="i"
+              class="todo"
+              @mouseover="
+                isShowEditIcon[item.category] = true
+                currentIndex = i
+              "
+              @mouseleave="
+                isShowEditIcon[item.category] = false
+                currentIndex = -1
+              "
+            >
+              <div class="todo__title">
+                <v-checkbox
+                  class="checkbox mt-0 pt-0"
+                  :off-icon="'mdi-checkbox-blank-circle-outline'"
+                  :on-icon="'mdi-check-circle-outline'"
+                  color="red darken-3"
+                  dense
+                  @click="
+                    $emit('finishedTodo', {
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                ></v-checkbox>
+                <label :class="{ finished: checkFinished(item) }">{{
+                  item.title
+                }}</label>
+              </div>
+
+              <div
+                v-show="isShowEditIcon[item.category] && currentIndex === i"
+                class="todo__menu"
+              >
+                <div
+                  class="edit-button"
+                  @click="
+                    showEditModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-lead-pencil</v-icon>
+                </div>
+                <div
+                  class="delete-button"
+                  @click="
+                    showDeleteModal({
+                      index: i,
+                      category: item.category,
+                    })
+                  "
+                >
+                  <v-icon class="todo__edit-button">mdi-delete</v-icon>
+                </div>
+              </div>
+            </li>
+          </draggable>
+          <AddButton @click="toggleModal('random')"></AddButton>
+        </div>
       </li>
     </ol>
     <AddModal
@@ -56,6 +254,14 @@
 export default {
   data() {
     return {
+      // DraggableTodoより
+      currentIndex: -1,
+      isShowEditIcon: {
+        work: false,
+        private: false,
+        random: false,
+      },
+      // 元々このvueファイル
       isShowAddModal: false,
       isShowEditModal: false,
       isShowDeleteModal: false,
@@ -90,9 +296,35 @@ export default {
     todoList() {
       return this.$store.state.todo.todoList
     },
+    draggableListWork: {
+      get() {
+        return this.todoList.work
+      },
+      set(value) {
+        console.log(value)
+        this.$store.commit('todo/updateDraggableList', value)
+      },
+    },
+    draggableListPrivate: {
+      get() {
+        return this.todoList.private
+      },
+      set(value) {
+        console.log(value)
+        this.$store.commit('todo/updateDraggableList', value)
+      },
+    },
+    draggableListRandom: {
+      get() {
+        return this.todoList.random
+      },
+      set(value) {
+        console.log(value)
+        this.$store.commit('todo/updateDraggableList', value)
+      },
+    },
   },
   methods: {
-    // ...mapActions('actionAddTodo'),
     selectAddCategory(value) {
       this.selectedAddCategory = value
     },
@@ -133,6 +365,10 @@ export default {
       this.selectedTodo.title = ''
       this.selectedTodo.detail = ''
       this.selectedTodo.category = ''
+    },
+    // Draggableから↓
+    checkFinished(target) {
+      return target.isFinished
     },
     finishedTodo(payload) {
       this.$store.dispatch('todo/actionFinishedTodo', payload)
@@ -189,5 +425,74 @@ ol {
   &__random {
     background-color: hsl(351, 50%, 77%);
   }
+}
+//以下TodoCategory.vueより
+.category-title {
+  font-weight: 600;
+  & label {
+    padding: 15px 20px 0 20px;
+    display: block;
+    font-size: 20px;
+    letter-spacing: 3px;
+  }
+}
+//以下DraggableTodo.vueより
+.todo {
+  background-color: white;
+  // opacity: 0.7;
+  text-align: center;
+  border-radius: 4px;
+  width: 90%;
+  display: flex;
+  // width: 100%;
+  margin: 6px auto 0;
+  padding: 5px 0 3px 5px;
+  justify-content: space-between;
+  align-items: baseline;
+  // text-align: center;
+  &__title {
+    margin-top: 0;
+    display: flex;
+    align-items: center;
+  }
+  &__edit-button {
+    // padding-right: 10px;
+  }
+  &:hover {
+  }
+  &__menu {
+    margin-right: 8px;
+  }
+}
+.edit-button,
+.delete-button {
+  display: inline-block;
+  margin-left: 0;
+}
+.finished {
+  position: relative;
+  &::before {
+    position: absolute;
+    top: 0.5em;
+    left: -0.3em;
+    content: '';
+    border: 2px solid #ad343e;
+    width: 200%;
+  }
+  // text-decoration: line-through #ad343e;
+}
+// .notyet {
+//   text-decoration: none;
+// }
+.checkbox {
+  display: inline-block;
+  margin-top: 0;
+  // height: 20px;
+}
+.v-messages {
+  display: none;
+}
+.input-slot {
+  margin-bottom: 0;
 }
 </style>
