43a4dd3f52d652992c6ab69a8c5742b77a9d20e9
高橋茉由 <>
2021-06-15 11:31:16 +0900

    lesson13 エラーページ完了

--- todo_initial/layouts/error.vue
+++ todo_initial/layouts/error.vue
@@ -1,12 +1,27 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
+    <p class="error-code">エラーコード：{{ error.statusCode }}<br /></p>
+    <div v-if="error.statusCode === 404" class="message-wrapper">
+      <h1>
+        {{ pageNotFound }}
+      </h1>
+      <NuxtLink to="/"> ホームに戻る </NuxtLink>
+    </div>
+    <div v-if="error.statusCode === 504" class="message-wrapper">
+      <h1>
+        {{ serverError }}
+      </h1>
+      <NuxtLink to="#"> 再度アクセスする </NuxtLink>
+    </div>
+    <div
+      v-if="error.statusCode !== 404 && error.statusCode !== 504"
+      class="message-wrapper"
+    >
+      <h1>
+        {{ otherError }}
+      </h1>
+      <NuxtLink to="/"> ホームに戻る </NuxtLink>
+    </div>
   </v-app>
 </template>
 
@@ -21,11 +36,14 @@ export default {
   },
   data() {
     return {
-      pageNotFound: '404 Not Found',
+      pageNotFound: 'ページが見つかりませんでした。',
+      serverError:
+        'アクセスできませんでした。\n時間をおいてから再度アクセスしてください。',
       otherError: 'An error occurred',
     }
   },
   head() {
+    // headタグのtitleタグを設定している
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
@@ -36,7 +54,31 @@ export default {
 </script>
 
 <style scoped>
+.error-code {
+  font-size: 18px;
+  margin-top: 50px;
+  text-align: center;
+}
 h1 {
-  font-size: 20px;
+  font-size: 24px;
+  text-align: center;
+}
+.theme--light .v-application {
+  background-color: #bc8089;
+}
+.message-wrapper {
+  text-align: center;
+  /* transition: all 100ms 0s linear; */
+}
+a {
+  display: block;
+  margin-top: 15px;
+  color: white;
+  opacity: 0.7;
+  text-decoration: none;
+}
+a:hover {
+  opacity: 1;
+  font-weight: bold;
 }
 </style>
