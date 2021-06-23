47d1b7fc320bc92b00e1c2ae0feb610c0f8292e3
高橋茉由 <>
2021-06-23 19:14:03 +0900

    Revert "テストブランチ作成。書き始め"
    This reverts commit f2953a9f24a02897f92b623ff8bf44879210b343.

--- test/Logo.spec.js
+++ test/Logo.spec.js
@@ -1,9 +1,9 @@
-// import { mount } from '@vue/test-utils'
-// import AddButton from '@/components/AddButton'
+import { mount } from '@vue/test-utils'
+import Logo from '@/components/Logo.vue'
 
-// describe('AddButton', () => {
-//   test('is a Vue instance', () => {
-//     const wrapper = mount(AddButton)
-//     expect(wrapper.vm).toBeTruthy()
-//   })
-// })
+describe('Logo', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(Logo)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
